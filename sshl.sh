b06ea642781653a8e3f4e2fffb60eff4ead1e3f9
Qurtin <37319086+Quist0n@users.noreply.github.com>
2020-06-26 04:58:26 -0500

    Added the ability to specify additional options for ssh at the command-line. Also added a message to show which ssh server is being connected to and with which options in case someone may find that interesting.

--- sshl.sh
+++ sshl.sh
@@ -7,16 +7,26 @@
 # Change the FILE variable pointing to the file containing the ssh command you want to execute
 # E.G you want to connect to a server on port 11 and the address is 10.13.12.2 and the user is bob you would put:
 #   bob@10.13.12.2 -p 11;
-# and so on in that same format with all your ssh servers. If you want to add comments,
+# and so on in that same format with all your ssh servers.
+
+# If you wish to specify addional options for the ssh to use, simply specify them when you run the script and they will be added in
+# Be sure not to make contradictions or specify an option twice by accident, since this will cause issues. The options in the file and the
+# options specified on the command-line are not checked in anyway so it may break a few things and ssh may be very angry with you.
+
+# If you want to add comments,
 # put a semicolon at the end of your server listing and then a # symbol followed by your comment.
 # This is good for naming servers to remember them. These are single-line ONLY! 
 # Do be careful to not put ; or # in your comment as well, has been proven to break string manipulation
 
-FILE="$HOME/.ssh_hosters"
+FILE="$HOME/.ssh_hosters";
+MESSAGE="Connecting to SSH server with command: ssh";
+
 #while loop which taking in the FILE var and prints to the screen
+
 cat -n $FILE
 read -r INPUT
+
 #var to use to make sure the INPUT is not a letter
 MATCH="^[abcdefghijklmnopqrstuvwxyz]$/i"
  ! [[ $INPUT =~ $MATCH ]]
-        DET=$(sed --regex -n "$INPUT"p "$FILE" | sed --regex -e 's/;\s*.*#.*// g') && exec ssh $DET
+        DET=$(sed --regex -n "$INPUT"p "$FILE" | sed --regex -e 's/;\s*.*#.*// g') && echo "$MESSAGE $DET $@" && ssh $DET $@;
