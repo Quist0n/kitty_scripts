fd4349c17e5167068007a361a509593edc39200a
Qurtin <37319086+Quist0n@users.noreply.github.com>
2020-07-10 22:33:44 -0500

    Fixed minor spelling mistakes

--- sshl.sh
+++ sshl.sh
@@ -2,14 +2,14 @@
 
 ## Author: Qurtin
 # This is a small script to read from a file so that I don't have to copy-paste the address of ssh servers....so I guess I am addicted to SSH
-# and linux and bash and scripting at this point.
+# and Linux and bash and scripting at this point.
 
 # Change the FILE variable pointing to the file containing the ssh command you want to execute
 # E.G you want to connect to a server on port 11 and the address is 10.13.12.2 and the user is bob you would put:
 #   bob@10.13.12.2 -p 11;
 # and so on in that same format with all your ssh servers.
 
-# If you wish to specify addional options for the ssh to use, simply specify them when you run the script and they will be added in
+# If you wish to specify additional options for the ssh to use, simply specify them when you run the script and they will be added in
 # Be sure not to make contradictions or specify an option twice by accident, since this will cause issues. The options in the file and the
 # options specified on the command-line are not checked in anyway so it may break a few things and ssh may be very angry with you.
 
