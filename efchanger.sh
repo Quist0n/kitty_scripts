0b80811c68671307133dbd25a110e4a783441805
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-01-30 01:12:44 -0500

    Major rewrite of efchanger.sh. Now allows you to input what you want to set the file permissions to and specify a directory to act upon.

--- efchanger.sh
+++ efchanger.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 #Little shell script to make all files in a directory and all its subdirectories no longer have
-#execute permissions, this is good for large media folders. Just run the script and all the files
-#will no longer be executable. The script has issues with directories and files with spaces in
-#them, so replace them with underscores (_). 
 
-#Just run the script in the directory and you wish for it to act on and then you are okay.
+#Just run the script, give the file mode you want to give (e.g 0700)
+# and the directory you wish for it to act
 
-FIL=$(find . -type f -name "*"); for LINE in $FIL; do chmod -c -x $LINE; done;
+find "$2" -type f -name "*" -exec chmod "$1" -c "{}" +;
