075da6c621dcaf28f274eb3904fde1bea45d6701
Quist0n <37319086+Quist0n@users.noreply.github.com>
2022-06-16 03:12:42 -0500

    Make menu command generic, add flag to change presets file

--- kitty_runner/kitty_runner.sh
+++ kitty_runner/kitty_runner.sh
@@ -1,32 +1,53 @@
 #!/bin/sh
 
-FILE="$XDG_CONFIG_HOME/kitty_runner/presets";
+PRESET_FILE="$XDG_CONFIG_HOME/kitty_runner/presets";
 MESSAGE="Loading preset";
-NUM_SHOWN_ENTRIES=8
+MENU_COMMAND="dmenu -l 5 -i"
+MODE="normal_mode"
+
+err(){
+        echo "$1"; exit 1
+}
 
 dmenu_mode(){
-        DET=$(cat "$FILE" | dmenu -l $NUM_SHOWN_ENTRIES -i -p "Choose program preset" | sed 's/;\s*#.*// g');
+        DET=$(cat "$PRESET_FILE" | $MENU_COMMAND | sed 's/;\s*#.*// g');
         echo "$MESSAGE $DET" "$@";
 }
 
 normal_mode(){
-        cat -n $FILE;
+        cat -n "$PRESET_FILE";
         read -r INPUT;
-        DET=$(sed -n "$INPUT"p "$FILE" | sed 's/;\s*#.*// g');
+        DET=$(sed -n "$INPUT"p "$PRESET_FILE" | sed 's/;\s*#.*// g');
 }
+
 if [ "$1" ]; then
 while [ "$1" ]; do
         case "$1" in
-                --dmenu-mode )
-                        dmenu_mode; shift;;
+                --dmenu-mode | -d )
+                        MODE="dmenu_mode"; shift;;
+                --normal-mode | -n )
+                        MODE="normal_mode"; shift;;
+                -p | --preset-file )
+                        shift;
+                        if [ ! -e "$1" ]; then
+                                err "Presets file $1 does not exist"
+
+                        else
+                                PRESET_FILE="$1"
+                                shift
+
+                        fi
+                        ;;
+
                 * )
                         shift; break;;
 
         esac
 done
-else
-        normal_mode
 fi
+
+echo "Using Presets file: $PRESET_FILE"
+$MODE
 if [ -z "$DET" ]; then
         printf "Plese select a preset";
 else
